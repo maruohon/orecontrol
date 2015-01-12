d86f1cb098c1c94e4181619c65e842a0d4b24139
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2015-01-12 15:33:45 +0200

    Add config options to fine tune what is allowed to generate.
    Note: Emerald and Monster Egg disabling hasn't been implemented yet.

--- src/main/java/fi/dy/masa/orecontrol/OreControl.java
+++ src/main/java/fi/dy/masa/orecontrol/OreControl.java
@@ -1,12 +1,15 @@
 package fi.dy.masa.orecontrol;
 
+import java.io.File;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
+import fi.dy.masa.orecontrol.config.Configs;
 import fi.dy.masa.orecontrol.eventhandler.OreGenEventHandler;
 import fi.dy.masa.orecontrol.reference.Reference;
 
@@ -18,22 +21,26 @@ public class OreControl
     public static OreControl instance;
 
     public static org.apache.logging.log4j.Logger logger;
+    public static File configFile;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         instance = this;
         logger = event.getModLog();
+        configFile = event.getSuggestedConfigurationFile();
+        //Configs.loadConfigs(event.getSuggestedConfigurationFile());
         MinecraftForge.ORE_GEN_BUS.register(new OreGenEventHandler());
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
+    }
+
+    @EventHandler
+    public void onServerAboutToStart(FMLServerAboutToStartEvent event)
+    {
+        Configs.loadConfigs(new File(configFile.getAbsolutePath()));
     }
 }
--- src/main/java/fi/dy/masa/orecontrol/eventhandler/OreGenEventHandler.java
+++ src/main/java/fi/dy/masa/orecontrol/eventhandler/OreGenEventHandler.java
@@ -4,6 +4,8 @@ import net.minecraftforge.event.terraingen.OreGenEvent;
 import net.minecraftforge.event.terraingen.OreGenEvent.GenerateMinable.EventType;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import fi.dy.masa.orecontrol.config.Configs;
+
 
 public class OreGenEventHandler
 {
@@ -11,12 +13,76 @@ public class OreGenEventHandler
     public void onOreGenMinable(OreGenEvent.GenerateMinable event)
     {
         //OreControl.logger.info("Type: " + event.type.toString() + (event.generator != null ? " generator: " + event.generator.toString() : ""));
+        //OreControl.logger.info("Type: " + event.type + " worldX: " + event.worldX + " worldZ: " + event.worldZ);
 
-        // TODO Add configs to fine tune what is allowed to generate
-        if (event.type != EventType.DIRT && event.type != EventType.GRAVEL && event.type != EventType.CUSTOM)
+        if (event.type == EventType.CUSTOM)
         {
-            //System.out.println("DENIED!!");
+            return;
+        }
+
+        if (Configs.disableRegularVanillaGen == true)
+        {
             event.setResult(Event.Result.DENY);
+            return;
+        }
+
+        switch(event.type)
+        {
+            case DIRT:
+                if (Configs.disableDirt == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            case GRAVEL:
+                if (Configs.disableGravel == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            case COAL:
+                if (Configs.disableCoal == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            case DIAMOND:
+                if (Configs.disableDiamond == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            case GOLD:
+                if (Configs.disableGold == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            case IRON:
+                if (Configs.disableIron == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            case LAPIS:
+                if (Configs.disableLapis == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            case REDSTONE:
+                if (Configs.disableRedstone == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            case QUARTZ:
+                if (Configs.disableNetherQuartz == true)
+                {
+                    event.setResult(Event.Result.DENY);
+                }
+                break;
+            default:
         }
     }
 }
