f629718851bca0bd3b19fe8fbbf3a398de9b3ddc
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2015-03-27 16:19:24 +0200

    Clean up the config class a bit.

--- src/main/java/fi/dy/masa/orecontrol/config/Configs.java
+++ src/main/java/fi/dy/masa/orecontrol/config/Configs.java
@@ -8,22 +8,6 @@ import fi.dy.masa.orecontrol.OreControl;
 
 public class Configs
 {
-    public static Property confDisableRegularVanillaGen;
-    public static Property confDisableRegularVanillaOres;
-    public static Property confDisableCustom;
-    public static Property confDisableDirt;
-    public static Property confDisableGravel;
-    public static Property confDisableCoal;
-    public static Property confDisableDiamond;
-    public static Property confDisableGold;
-    public static Property confDisableIron;
-    public static Property confDisableLapis;
-    public static Property confDisableRedstone;
-    public static Property confDisableNetherQuartz;
-    public static Property confDisableEmerald;
-    public static Property confDisableMonsterEgg;
-
-
     public static boolean disableRegularVanillaGen;
     public static boolean disableRegularVanillaOres;
     public static boolean disableCustom;
@@ -44,76 +28,72 @@ public class Configs
     {
         OreControl.logger.info("Loading configuration...");
 
-        String category;
         Configuration conf = new Configuration(configFile);
         conf.load();
 
-        category = "vanilla";
-        confDisableRegularVanillaGen = conf.get(category, "disableRegularVanillaGen", false);
-        confDisableRegularVanillaGen.comment = "Disables all \"regular\" vanilla generation. NOTE: This includes Dirt and Gravel pockets inside stone, but not Emeralds in Extreme Hills biomes.";
+        Property prop;
+        String category = "vanilla";
 
-        confDisableRegularVanillaOres = conf.get(category, "disableRegularVanillaOres", false);
-        confDisableRegularVanillaOres.comment = "Disables \"regular\" vanilla _ore_ generation, including Nether Quartz. This does not include Emeralds in Extreme Hills biomes, which are not part of the regular ore generation.";
+        prop = conf.get(category, "disableRegularVanillaGen", false);
+        prop.comment = "Disables all \"regular\" vanilla generation. NOTE: This includes Dirt and Gravel pockets inside stone, but not Emeralds in Extreme Hills biomes.";
+        disableRegularVanillaGen = prop.getBoolean();
 
-        confDisableCustom = conf.get(category, "disableCustom", false);
-        confDisableCustom.comment = "Disables custom ore generation. NOTE: Very few mods actually use the OreGenEvent.GenerateMinable event though, and this only works with those that do.";
+        prop = conf.get(category, "disableRegularVanillaOres", false);
+        prop.comment = "Disables \"regular\" vanilla _ore_ generation, including Nether Quartz. This does not include Emeralds in Extreme Hills biomes, which are not part of the regular ore generation.";
+        disableRegularVanillaOres = prop.getBoolean();
 
-        confDisableDirt = conf.get(category, "disableDirt", false);
-        confDisableDirt.comment = "Disables Dirt pocket generation inside stone.";
+        prop = conf.get(category, "disableCustom", false);
+        prop.comment = "Disables custom ore generation. NOTE: Very few mods actually use the OreGenEvent.GenerateMinable event though, and this only works with those that do.";
+        disableCustom = prop.getBoolean();
 
-        confDisableGravel = conf.get(category, "disableGravel", false);
-        confDisableGravel.comment = "Disables Gravel pocket generation inside stone.";
+        prop = conf.get(category, "disableDirt", false);
+        prop.comment = "Disables Dirt pocket generation inside stone.";
+        disableDirt = prop.getBoolean() || disableRegularVanillaGen;
 
-        confDisableCoal = conf.get(category, "disableCoal", false);
-        confDisableCoal.comment = "Disables Coal Ore generation.";
+        prop = conf.get(category, "disableGravel", false);
+        prop.comment = "Disables Gravel pocket generation inside stone.";
+        disableGravel = prop.getBoolean() || disableRegularVanillaGen;
 
-        confDisableDiamond = conf.get(category, "disableDiamond", false);
-        confDisableDiamond.comment = "Disables Diamond Ore generation.";
+        prop = conf.get(category, "disableCoal", false);
+        prop.comment = "Disables Coal Ore generation.";
+        disableCoal = prop.getBoolean() || disableRegularVanillaOres;
 
-        confDisableGold = conf.get(category, "disableGold", false);
-        confDisableGold.comment = "Disables Gold Ore generation.";
+        prop = conf.get(category, "disableDiamond", false);
+        prop.comment = "Disables Diamond Ore generation.";
+        disableDiamond = prop.getBoolean() || disableRegularVanillaOres;
 
-        confDisableIron = conf.get(category, "disableIron", false);
-        confDisableIron.comment = "Disables Iron Ore generation.";
+        prop = conf.get(category, "disableGold", false);
+        prop.comment = "Disables Gold Ore generation.";
+        disableGold = prop.getBoolean() || disableRegularVanillaOres;
 
-        confDisableLapis = conf.get(category, "disableLapis", false);
-        confDisableLapis.comment = "Disables Lapis Ore generation.";
+        prop = conf.get(category, "disableIron", false);
+        prop.comment = "Disables Iron Ore generation.";
+        disableIron = prop.getBoolean() || disableRegularVanillaOres;
 
-        confDisableRedstone = conf.get(category, "disableRedstone", false);
-        confDisableRedstone.comment = "Disables Redstone Ore generation.";
+        prop = conf.get(category, "disableLapis", false);
+        prop.comment = "Disables Lapis Ore generation.";
+        disableLapis = prop.getBoolean() || disableRegularVanillaOres;
 
-        confDisableNetherQuartz = conf.get(category, "disableNetherQuartz", false);
-        confDisableNetherQuartz.comment = "Disables Nether Quartz Ore generation.";
+        prop = conf.get(category, "disableNetherQuartz", false);
+        prop.comment = "Disables Nether Quartz Ore generation.";
+        disableNetherQuartz = prop.getBoolean() || disableRegularVanillaOres;
 
-        confDisableEmerald = conf.get(category, "disableEmerald", false);
-        confDisableEmerald.comment = "Disables Emerald Ore generation in Extreme Hills biomes. NOTE: To achieve this, the BiomeGenHills generator will be replaced.";
+        prop = conf.get(category, "disableRedstone", false);
+        prop.comment = "Disables Redstone Ore generation.";
+        disableRedstone = prop.getBoolean() || disableRegularVanillaOres;
 
-        confDisableMonsterEgg = conf.get(category, "disableMonsterEgg", false);
-        confDisableMonsterEgg.comment = "Disables Monster Egg (= Silverfish block) generation in Extreme Hills biomes. NOTE: To achieve this, the BiomeGenHills generator will be replaced.";
+        // Special ores, not part of regular ore generation
+        prop = conf.get(category, "disableEmerald", false);
+        prop.comment = "Disables Emerald Ore generation in Extreme Hills biomes. NOTE: To achieve this, the BiomeGenHills generator will be replaced.";
+        disableEmerald = prop.getBoolean();
+
+        prop = conf.get(category, "disableMonsterEgg", false);
+        prop.comment = "Disables Monster Egg (= Silverfish block) generation in Extreme Hills biomes. NOTE: To achieve this, the BiomeGenHills generator will be replaced.";
+        disableMonsterEgg = prop.getBoolean();
 
         if (conf.hasChanged() == true)
         {
             conf.save();
         }
-
-        setValuesFromProperties();
-    }
-
-    public static void setValuesFromProperties()
-    {
-        disableRegularVanillaGen    = confDisableRegularVanillaGen.getBoolean();
-        disableRegularVanillaOres   = confDisableRegularVanillaOres.getBoolean();
-        disableCustom               = confDisableCustom.getBoolean();
-        disableDirt                 = disableRegularVanillaGen || confDisableDirt.getBoolean();
-        disableGravel               = disableRegularVanillaGen || confDisableGravel.getBoolean();
-        disableCoal                 = disableRegularVanillaOres || confDisableCoal.getBoolean();
-        disableDiamond              = disableRegularVanillaOres || confDisableDiamond.getBoolean();
-        disableGold                 = disableRegularVanillaOres || confDisableGold.getBoolean();
-        disableIron                 = disableRegularVanillaOres || confDisableIron.getBoolean();
-        disableLapis                = disableRegularVanillaOres || confDisableLapis.getBoolean();
-        disableRedstone             = disableRegularVanillaOres || confDisableRedstone.getBoolean();
-        disableNetherQuartz         = disableRegularVanillaOres || confDisableNetherQuartz.getBoolean();
-        disableEmerald              = confDisableEmerald.getBoolean();
-        disableMonsterEgg           = confDisableMonsterEgg.getBoolean();
     }
 }
