a2c53d309c4cddd6e7eaf787111528b0b8620c44
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2015-11-29 17:09:35 +0200

    Update to Minecraft 1.8.8 (Forge 11.15.0.1595 beta)

--- src/main/java/fi/dy/masa/orecontrol/reference/Reference.java
+++ src/main/java/fi/dy/masa/orecontrol/reference/Reference.java
@@ -5,6 +5,4 @@ public class Reference
     public static final String MOD_ID = "orecontrol";
     public static final String MOD_NAME = "Ore Control";
     public static final String MOD_VERSION = "@MOD_VERSION@";
-    //public static final String PROXY_CLASS_CLIENT = "fi.dy.masa.toomanyores.proxy.ClientProxy";
-    //public static final String PROXY_CLASS_SERVER = "fi.dy.masa.toomanyores.proxy.ServerProxy";
 }
