2ba86aa7b53c4e544f8b6e4b9c0b5dd8e43c1625
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-05-19 15:32:07 +0300

    Update to Minecraft 1.9.4

--- src/main/java/fi/dy/masa/orecontrol/OreControl.java
+++ src/main/java/fi/dy/masa/orecontrol/OreControl.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
-    acceptableRemoteVersions="*", acceptedMinecraftVersions = "1.9")
+    acceptableRemoteVersions="*", acceptedMinecraftVersions = "1.9,1.9.4")
 public class OreControl
 {
     @Instance(Reference.MOD_ID)
