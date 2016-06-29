855bbaa8e8152ab476f3ceef014cc95f6fe88034
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-06-29 16:27:27 +0300

    Update to Minecraft 1.10.2

--- src/main/java/fi/dy/masa/orecontrol/OreControl.java
+++ src/main/java/fi/dy/masa/orecontrol/OreControl.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
-    acceptableRemoteVersions="*", acceptedMinecraftVersions = "[1.9,1.10]")
+    acceptableRemoteVersions="*", acceptedMinecraftVersions = "[1.9,1.10.2]")
 public class OreControl
 {
     @Instance(Reference.MOD_ID)
